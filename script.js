482df16c352559831c823fa35ff70467e57b924a
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-01-31 14:26:00 +0100

    getting all episodes from provided file

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 //You can edit ALL of the code here
+const root = document.getElementById("root");
+
+// Get all episodes from provided file
+const episodes = getAllEpisodes();
+
+// Create page title
+const title = document.createElement("h1");
+title.textContent = "TV Show Episodes";
+root.appendChild(title);
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
